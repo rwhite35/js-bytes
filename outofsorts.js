438e70d493a498b5c42387a269ce8deaa352fe66
rwhite53 <rwhite@october-design.com>
2024-08-25 11:27:32 -0400

    WIP: step two complete, work arrays reordered

--- outofsorts.js
+++ outofsorts.js
@@ -13,61 +13,66 @@ const numberSet = [11,1,21,3,4,32,103,7,4,56,2,12];
 
 
 /**
- * helper: evaluates left-hand-side as greater than right-hand-side.
- * if lhs isnt greater than, returns right-hand-side.
+ * helper: evaluates left-hand-side for greater than right-hand-side.
+ * @param {Number} lhs list value being evaluated
+ * @param {Number} rhs value to compare lhs against
+ * @returns {Boolean} true when lhs is greater than rhs.
  */
-function leftIsGreater(lhs, rhs) {
-    console.log(`${leftIsGreater.name} received values left ${lhs}, right ${rhs}`);
+function lhsIsGreater(lhs, rhs) {
+    // console.log(`${lhsIsGreater.name} comparing vals lhs ${lhs} to rhs ${rhs}`);
     return (lhs > rhs) ?? rhs;
 }
 
 
 /**
- * sort high to low
+ * worker: uses an Insertion Sort algorithm to order a list of Numbers 
+ * which are assigned in descending order to a return array.
+ * @param {Array} list un ordered list of numbers
+ * @returns {Array} list numbers in descending order
  */
 function sortList(list = new Array, vector = new Array) {
+    // outter loop over all list elements
     const incr = 1;
-    // loop through each list element
     for (let i = 0; i < list.length; i++) {
-        const idx = [i - incr, i, i + incr];    // tuple index[prev, left, right]
-        const lhs = list[idx[1]];               // current Number to eval
-        const rhs = list[idx[2]];               // next Number to compare
-        console.log(`${sortList.name} current ${idx[1]} list has lhs ${lhs}, rhs ${rhs}!`);
+        const idx = [i - incr, i, i + incr];    // rolling index pointers [current, left, right]
+        const lhs = list[idx[1]];               // Number to evaluate and assign
+        // console.log(`${sortList.name} outter loop ${idx[1]} for lhs value ${lhs}!`);
 
-        // lookback at previous saved value vs rhs value.
-        // assumes last loop pushed the next highest value by ascending order.
-        if (vector.length >= 1) {
-            let save = vector[idx[0]];
-            let tuple = [,,];
-            tuple[0] = (leftIsGreater(save,lhs) && leftIsGreater(save, rhs)) ? save : (leftIsGreater(lhs,rhs)) ? lhs : rhs;
-            tuple[1] = (leftIsGreater(lhs,save)) ? lhs : save;
-            tuple[2] = lhs;
-            console.log(`- work: concat vector with tuple values ${tuple}`);
+        if (vector.length == 0) {
+            vector.push(lhs); // first time thru
 
-            vector.concat(tuple);
+        } else { // insert value in sequential order
+            let lastIdx = vector.length - incr; // ensures its last element added
+            if (lhsIsGreater(lhs,vector[0])) {
+                vector.unshift(lhs);    // at beginning
 
-        } else { // first time thru, push lhs onto vector stack
-            vector.push(lhs);
+            } else if (!lhsIsGreater(lhs,vector[lastIdx])) {
+                vector.push(lhs);       // at end
+
+            } else { // needle in haystack, 
+                // this using a `next to` strategy - is the next number greater than lhs?
+                if (vector.length >= 3) {
+                    vector = vector.flatMap( (elm) => (lhs > elm && !vector.includes(lhs)) ? [lhs,elm] : elm );
+                }
+            }
         }
-        console.log(vector);
+        // done. let see what we've got...
+        // console.log(vector);
     }
-
-    console.log(`- done: returns sorted list as vector`);
     return vector;
 }
 
 
 /**
- * helper: takes input array and chunks it into smaller array 
- * @param {any[]} inArr one dimensional array of of unordered numbers
- * @return {array} workArr array with two or more sub arrays mapped from inArr.
+ * helper: takes input array and chunks it into smaller work arrays. 
+ * @param {Array} inArr one dimensional array of random numbers (integers).
+ * @return {[Array],Number} one or more work arrays chunked from input array, last element is divisor for reference.
  */
 function chunkWorkArray(inArr, workArr = new Array) {
     const totElms = inArr.length;
     const divisor = totElms >= 100 ? 4 : 2;
     const multiple = Math.abs(totElms / divisor);
-    console.log(`${chunkWorkArray.name} working...`);
-    console.log(` inArr.length: ${totElms}, divisor: ${divisor}, index multiple: ${multiple}`);
+    console.log(`${chunkWorkArray.name} received array with ${totElms} numbers to chunk!`);
 
     let delta = divisor;
     while (delta > 0) { // chunk inArr into two or four sub arrays
@@ -76,46 +81,48 @@ function chunkWorkArray(inArr, workArr = new Array) {
         workArr.push(inArr.slice(start,end));
         delta = delta - 1;
     }
-    // add divisor as last element for convenient access
+
+    // add divisor for reference, would be same as work array count.
     workArr.push(divisor);
+
     console.log(`done chunking workArr: ${workArr}`);
     return workArr;
 }
 
 
-/** algo: Quicksort - probably the most common sorting algorithm.
- * the process divides and conquers by splitting the work into chucks.
- * then through multiple rounds of comparing one value to another,
- * orders a random set of numbers into an ordered list (asc or desc).
- * The time complexity worst case is O(n^2).
- * @param {*} inArr unordered array of numbers
- * @param {String} order sort order is `asc`ending (default), or `desc`ending
- * @returns {Array} list of numbers in ascending or descending order
+/** 
+ * method: Quicksort using an insertion algorithm.
+ * first the process divides and conquers, splitting work into chucks;
+ * next re-order each work array from high to low (descending);
+ * finally merges work arrays into final list of sorted numbers.
+ * 
+ * @param {[Number]} inArr required, unordered list of integers
+ * @returns {[Number]} sorted list of numbers in descending order
+ * time complexity worst case is O(n^2)
  */
-function goQuicksort(inArr, order = 'asc', outArr = new Array) {
+function goQuicksort(inArr, outArr = new Array) {
     let resetNull = null;
     let inCnt = inArr.length;
-    console.log(`${goQuicksort.name} array arg. length ${inCnt}`);
+    console.log(`${goQuicksort.name} has array of ${inCnt} length`);
 
     if (typeof inArr == 'undefined' || inCnt === 0) return outArr;
 
-    // first: break work into smaller chunks
-    let tempArr = chunkWorkArray(inArr);
-    let divisor = tempArr.pop();
-    console.log(`tempArr.length ${tempArr.length}, divisor ${divisor}`);
+    // first: break work into chunks, 
+    // - calls chunkWorkArray(array) passing inArr argument
+    let workArr = chunkWorkArray(inArr);
+    let divisor = workArr.pop();
+    // console.log(`tempArr.length ${tempArr.length}, divisor ${divisor}`);
 
-    // next: evaluate each array chuck using sortList() for comparison
-    let vector = [];
+    // next: process chucks using sortList()
+    // - calls sortList(list) passing this workArr[i] as argument
     for (let i = 0; i < divisor; i++) { // outter array 
-        let list = tempArr[i];          // inner array proto[1,32,56,..]
-        vector = sortList(list);
-        
-        // last, concat this vector into workArr and reset vars for next outter loop
-        console.log(` - done: finally, concat this vector ${vector} onto workArray`);
-        outArr.concat(vector);
-        vector = []
-        list = resetNull;
+        let list = workArr[i];          // inner array proto[1,32,56,..]
+        workArr[i] = sortList(list);    // re - assign at current position
     }
+    console.log(`- workArr reordered numbers completed`);
+    console.log(workArr);
+
+    // finally: merge workArrs into one output array
     return outArr
 }
 
