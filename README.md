779fa65411212a92f96ab86ae4364f9df8a03c32
rwhite53 <rwhite@october-design.com>
2024-08-25 14:32:17 -0400

    WIP nearly there, but take a break

--- README.md
+++ README.md
@@ -25,4 +25,8 @@ NOTE: MacOS 14 and Homebrew's Node distro may have conflicts with other system r
    `function mandelBrotsCurse(inArr, outArr = new Array) {}`
    Traverse through a number set array of unknown size and structure to return only composite (three or more factors) numbers in a new array. The return array can be undefined (default), empty, or has one or more numbers. This method has a time complexity of O^1 or constant time.
 
+3. [outofsorts.js] (https://github.com/rwhite35/js-bytes/blob/master/outofsorts.js)<br />
+   Example of Insertion Sort algorithm for reordering a list of number having an unknown length and range. It builds on some of the traversing work in composite.js, but includes logic for handling different situations. Including chucking larger work into small work, sorting and merge data, and handling issues like undefined or empty values.
+   NOTE: its still a work in progress. The mock data looks like `11,1,21,3,4,32,103,7,4,56,2,12,2` and the end data (as of commit ) is `103,32,56,4,3,56,1,4`. Getting there, but still needs some work.
+
 Enjoy!
--- outofsorts.js
+++ outofsorts.js
@@ -19,14 +19,49 @@ const numberSet = [11,1,21,3,4,32,103,7,4,56,2,12];
  * @returns {Boolean} true when lhs is greater than rhs.
  */
 function lhsIsGreater(lhs, rhs) {
-    // console.log(`${lhsIsGreater.name} comparing vals lhs ${lhs} to rhs ${rhs}`);
     return (lhs > rhs) ?? rhs;
 }
 
 
 /**
+ * helper: merger mainia
+ * @return {Boolean} true on completed successfully
+ */
+function mergerMania(val, tuple, outArr, success = Boolean) {
+    if (typeof val === undefined || val.length <= 0) {
+        throw new Error(`val argumentis empty or undefined!`);
+    }
+    // console.log(`${mergerMania.name} for val ${val}, with outArr ${outArr}, and tuple ${tuple}`);
+
+    if (val > tuple[0]) { // a new high
+        if (!outArr.includes(val)) outArr.unshift(val);
+        tuple[2] = tuple[1];
+        tuple[1] = tuple[0];
+        tuple[0] = val;
+
+    } else if (val < tuple[2]) { // a new low
+        let bidx = outArr.indexOf(tuple[2]);
+        if (!outArr.includes(val)) outArr[bidx] = val;
+        tuple[2] = val;
+
+    } else if(val > tuple[1] && val < tuple[0]) { // top half
+        let midx = outArr.indexOf(tuple[1]);
+        if (!outArr.includes(val)) outArr[midx + 1] = val;
+        tuple[1] = val;
+
+    } else { // bottom half
+        outArr.push(val);
+        tuple[2] = val;
+    }
+
+    // console.log(`${mergerMania.name} ending outArr ${outArr}, tuple ${tuple}`);
+    return success = true;
+}
+
+
+/**
  * worker: uses an Insertion Sort algorithm to order a list of Numbers 
- * which are assigned in descending order to a return array.
+ * which are assigned in descending order to a new return array.
  * @param {Array} list un ordered list of numbers
  * @returns {Array} list numbers in descending order
  */
@@ -36,10 +71,9 @@ function sortList(list = new Array, vector = new Array) {
     for (let i = 0; i < list.length; i++) {
         const idx = [i - incr, i, i + incr];    // rolling index pointers [current, left, right]
         const lhs = list[idx[1]];               // Number to evaluate and assign
-        // console.log(`${sortList.name} outter loop ${idx[1]} for lhs value ${lhs}!`);
 
-        if (vector.length == 0) {
-            vector.push(lhs); // first time thru
+        if (vector.length == 0) { // first time thru
+            vector.push(lhs);
 
         } else { // insert value in sequential order
             let lastIdx = vector.length - incr; // ensures its last element added
@@ -64,7 +98,7 @@ function sortList(list = new Array, vector = new Array) {
 
 
 /**
- * helper: takes input array and chunks it into smaller work arrays. 
+ * worker: takes input array and chunks it into smaller work arrays. 
  * @param {Array} inArr one dimensional array of random numbers (integers).
  * @return {[Array],Number} one or more work arrays chunked from input array, last element is divisor for reference.
  */
@@ -90,11 +124,43 @@ function chunkWorkArray(inArr, workArr = new Array) {
 }
 
 
+/**
+ * worker: merge sanitize workArr, returning one array of ordered numbers
+ */
+function mergeWorkArray(workArr, outArr) {
+
+    // workArr indexes for merging left-hand-side and right-hand-side
+    const wid = { lhs: 0, rhs: 1 }
+    const arrCnt = workArr.length
+    let outterLoop = (workArr[wid.lhs].length == workArr[wid.rhs].length) ? workArr[wid.lhs].length : workArr[wid.rhs].length;
+
+    // seeds tuple proxy with rolling value for outArr [0] high, [1] middle, [2] low
+    // note tuple[2] doesn't seed a floor value, that will happen in the first loop.
+    let tuple = [1,1,undefined];
+    outArr.join(tuple);
+
+    for (let i = 0; i < outterLoop; i++) {
+        console.log(`- loop ${i} workArr evals lhs ${workArr[wid.lhs][i]}, rhs ${workArr[wid.rhs][i]}`);
+        try {
+            let lhs  = (workArr[wid.lhs][i] !== undefined) ? workArr[wid.lhs][i] : [];
+            let rhs  = (workArr[wid.rhs][i] !== undefined) ? workArr[wid.rhs][i] : [];
+            // throws error
+            mergerMania(lhs, tuple, outArr);
+            mergerMania(rhs, tuple, outArr);
+        }
+        catch (err) {
+            console.log(`WARN: ${mergeWorkArray.name} mergerMainia issue! ${err}`);
+        }
+    }
+    return outArr;
+}
+
+
 /** 
- * method: Quicksort using an insertion algorithm.
- * first the process divides and conquers, splitting work into chucks;
- * next re-order each work array from high to low (descending);
- * finally merges work arrays into final list of sorted numbers.
+ * accessor method: wraps and organizes work into a three step process
+ * first: divide and conquer list of numbers into managable chucks of work;
+ * next: re-order each workArr from high to low (descending);
+ * finally: merge and sanitize workArr into a final list of sorted numbers.
  * 
  * @param {[Number]} inArr required, unordered list of integers
  * @returns {[Number]} sorted list of numbers in descending order
@@ -107,27 +173,27 @@ function goQuicksort(inArr, outArr = new Array) {
 
     if (typeof inArr == 'undefined' || inCnt === 0) return outArr;
 
-    // first: break work into chunks, 
-    // - calls chunkWorkArray(array) passing inArr argument
+    // first: call chunkWorkArray(array) with argument inArr
     let workArr = chunkWorkArray(inArr);
     let divisor = workArr.pop();
-    // console.log(`tempArr.length ${tempArr.length}, divisor ${divisor}`);
+    // console.log(`- step 1 done: chunkWorkArray() returned workArr.length ${workArr.length}, used divisor ${divisor}`);
 
-    // next: process chucks using sortList()
-    // - calls sortList(list) passing this workArr[i] as argument
-    for (let i = 0; i < divisor; i++) { // outter array 
-        let list = workArr[i];          // inner array proto[1,32,56,..]
-        workArr[i] = sortList(list);    // re - assign at current position
+    // next: call sortList(list) with each workArr[i] as argument
+    for (let i = 0; i < divisor; i++) {
+        workArr[i] = sortList(workArr[i]); // reassign at current position
     }
-    console.log(`- workArr reordered numbers completed`);
-    console.log(workArr);
+    console.log(`- step 2 done: sortList() completed, workArr re-ordered: ${workArr}`);
 
-    // finally: merge workArrs into one output array
+    // finally: merged work array into outArr for easy consumption.
+    // call mergeWorkArray(array) passing workArr and outArr as arguments
+    mergeWorkArray(workArr, outArr);
+    // console.log(`- step 3 done: mergeWorkArray() updated final outArr values: ${outArr}`);
+
     return outArr
 }
 
 sortedArr = goQuicksort(numberSet);
-console.log(`These numbers are sorted ${sortedArr}`);
+console.log(`These numbers were sorted ${sortedArr}`);
 
 
 
