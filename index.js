cd22531e6628dcff6651c0239dbca6b777ce156a
rwhite53 <rwhite@october-design.com>
2024-08-26 07:25:18 -0400

    WIP: output value to html using document.querySelector() and textContent() build in.

--- index.js
+++ index.js
@@ -3,40 +3,38 @@
 // //\\ Primitive Types
 //
 // const & var - global primitives
-const greeting = 'Hello';   // immutable, inits with value
-var timesTried = 1;         // mutable, inits with value
+const count     = 10;
+const greeting  = 'Hello';  // immutable, inits with value
+var timesTried  = 1;        // mutable, inits with value
+
 if(timesTried === 1) {
     var timesTried = 4;
     console.log(timesTried);
 }
-console.log('How many times tried? ' + timesTried);
 
 // let - dynamic type values can change
-let name = 'jello';         // String wrapper
-let age = 35;               // Number wrapper
-let happy = true;           // Boolean wrapper
-let maxSize = BigInt(Number.MAX_SAFE_INTEGER);
+let name        = 'jello';  // String wrapper
+let age         = 35;       // Number wrapper
+let happy       = true;     // Boolean wrapper
+let maxSize     = BigInt(Number.MAX_SAFE_INTEGER);
 let stateOf;                // undefined - is both a type and var default value
-let resetValue = null;      // is object with value of null
-console.log(greeting +' '+ name); // Hello jello
+let resetValue  = null;     // is object with value of null
 
 // `var` and `let` have the same behavior
 if(name === 'jello') {
     name = 'biafra';
     console.log(name);
 }
-console.log('What was that name? ' + name);
-
-// use `typeof varName` to check a variable type.
 name = resetValue;
-console.log('reset name to ' + typeof name); // object w/ null value
+
 
 // //\\ Reference Types
 //
 // Objects use curly braces
-let sizeObject = {}     // object literal
-const constObj = null;  // null object
-const nameProp = "name";
+let sizeObject  = {}        // object literal
+const constObj  = null;     // null object
+const nameProp  = "name";
+
 
 // Objects - 
 // uses keyword `let` or `const`, can have mutable properties.
@@ -45,15 +43,14 @@ const person = {
     age: 24
 }
 // ref value by square bracket or dot notation
-console.log(person.name);
 console.log(greeting +' '+ person[nameProp]);
 
 // Arrays -
 // Array is indexed, not associated key=value, Use Object(above) for that.
 // items(aka elements) can dynamically change type ie sting to number.
-let litArray = [];          // Array literal
-let otherNames = ['Tom','Janet']; // zero-indexed, [0] = Tom
-otherNames[2] = 'Peter';    // now length is three element long
+let litArray    = [];       // Array literal
+let otherNames  = ['Tom','Janet'];  // zero-indexed, [0] = Tom
+otherNames[2]   = 'Peter';  // now length is three element long
 
 // Array has built in methods like `forEach`, `every`, `find`, `length` etc
 otherNames.forEach((element) => console.log(element)); // Tom, Janet, Peter
@@ -61,10 +58,6 @@ litArray = [
     ["Tina",["Ralph","Stanley"]],
     ["Pam","Sammy"]
 ]
-// other usages
-console.log('litArray copy length is ' + litArray.length); // 2 elements
-console.log('litArray[0] length is ' + litArray[0].length); // 2 elements
-console.log('Favorite name ' + litArray[0][1][1]);  // Stanley
 
 // array-copy operations (ie from()) makes shallow copies of an array.
 // here, the litArray2 ref points to litArray, they are the same
@@ -75,7 +68,6 @@ console.log(litArray2);
 // here, a deep copy (removes ref pointer) is created
 litArray3 = JSON.parse(JSON.stringify(litArray));
 litArray[2] = "Steven"; // now 3 elements for both litArray & litArray2
-console.log('deep copy array length ' + litArray3.length); // 2
 
 
 // //\\ Functions -
@@ -90,11 +82,9 @@ function countElem(array) {
     }
     return count;
 }
-// pass `litArray` argument for parameter `array`
-console.log('array size ' + countElem(litArray)); // 3
-
-// GOTCHA: didn't recurse beyond the top level (3),
-// requires more logic to traverse all elements.
+// GOTCHA: is a shallow iteration over the top level elements.
+// recursions would require condition logic (ie. is element an array type?)
+// and another looping function for true conditions.
 
 // compute number of object properties
 Object.objsize = function(obj) { 
@@ -106,10 +96,45 @@ Object.objsize = function(obj) {
     } 
     return size; 
 };
-console.log("person size: " + Object.objsize(person));
+// console.log("person size: " + Object.objsize(person));
 
 // calling functions from function
 function square(number) {
     return number * number;
 }
-console.log(square(3)); // 9
+// console.log(square(3)); // 9
+
+
+// // \\ Loop Guru
+//
+// `for` loop evaluate the continue loop condition 
+// (ie. the after expression) at end of iteration 
+for (let i = 0; i < count; i++) {
+    console.log(`${i} gurus iterating!`); // 0,1,..9
+}
+
+// Its a lonely blue nowhere for `Lil Steven`. 
+// In this example `for` loops over 3 litArray[[0]:[], [1]:[], [2]:''] 
+// bracket level elms, and uses `constraint condition` to consoles only 
+// non-Array elements.
+let retort = document.querySelector("#notMyName");
+for (key in litArray) {
+    if (!Array.isArray(litArray[key])) {
+        let str = `But I'm Lil ${litArray[key]}! :-(`;
+        retort.textContent = str;
+    }
+} 
+
+// `while` loops evaluate the continue condition before the
+// next iteration. BONUS: `c` would count down (or up) in 
+// non-negative whole numbers. The current loop count value
+// is available at loop start for additional abuse. 
+let c = count;
+while (c > 0) {
+    console.log(`${c} gurus looping!`);
+    c--;    // 10,9,..1 
+}
+
+// similarly, `do...while` does the same thing, however, 
+// the statement is executed BEFORE the condition is checked.
+// in application terms, do...while behaves like a `for` loop.
