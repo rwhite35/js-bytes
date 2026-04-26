a299450134729ef5c5b7ef5cd1859118dc5ee40f
rwhite53 <rwhite@october-design.com>
2026-04-26 14:30:31 -0400

    UPDATE: leavetochance.js

--- leavetochance.js
+++ leavetochance.js
@@ -5,6 +5,7 @@
 // hitchhiking around the gallaxy...
 
 const trials = 12;
+const ceiling = 24;
 const headsCoin = document.querySelector("#heads");
 const tailsCoin = document.querySelector("#tails");
 const tossResult = document.querySelector("#tossResult");
@@ -12,17 +13,20 @@ const tossResult = document.querySelector("#tossResult");
 // value range is -24 to 24 for a 50/50 chance
 // that either even or odd number will result.
 function randomNumber(min,max) {
-    const minRev = Math.floor(min);
-    const maxRev = Math.ceil(max);
-    return Math.floor(Math.random() * (minRev - maxRev) + minRev);
+    const computed  = Math.ceil((min - max) + min);
+    const scaler    = (min % 2 == 0) ? computed : max;
+    console.log(`- randomNumber min ${min}, max ${max}, computed ${computed}, scaler ${scaler}!`);
+    return Math.floor(Math.random() * scaler)
 }
 
 function coinToss() {
     let headCnt = 0;
     let tailCnt = 0;
     for(let i = 0; i < trials; i++) {
-        let num = randomNumber(1,25);
+        let lowerCeiling = ceiling - i;
+        let num = randomNumber(i,lowerCeiling);
         let res = (num % 2 == 0) ? 'heads' : 'tails';
+        console.log(`- coinToss random num ${num}, coin face ${res.toUpperCase()}`);
         switch (res) {
             case 'heads': 
                 headCnt++;
@@ -37,10 +41,10 @@ function coinToss() {
     let resultStr = '';
     switch (result) {
         case 'Heads': 
-            resultStr = `${result} wins ${headCnt} out of 12 tosses!`;
+            resultStr = `${result} wins ${headCnt} out of ${trials} tosses!`;
             break;
         case 'Tails':
-            resultStr = `${result} wins ${tailCnt} out of 12 tosses!`;
+            resultStr = `${result} wins ${tailCnt} out of ${trials} tosses!`;
             break;
         case 'Tie':
             resultStr = `Yikes! Its a ${result}. Maybe decide this one on your own?`;
